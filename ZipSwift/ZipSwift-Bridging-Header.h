72e31cbfc392f80aeefaf3b50342761feef0f686
Rob Warner <rwarner@grailbox.com>
2015-02-18 16:00:18 -0500

    Add bridging header

---
+++ ZipSwift/ZipSwift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ZipSwift-Bridging-Header.h
+//  ZipSwift
+//
+//  Created by Rob Warner on 2/18/15.
+//  Copyright (c) 2015 Coding in Shades. All rights reserved.
+//
+
+#ifndef ZipSwift_ZipSwift_Bridging_Header_h
+#define ZipSwift_ZipSwift_Bridging_Header_h
+
+#import <RestKit/RestKit.h>
+
+#endif
