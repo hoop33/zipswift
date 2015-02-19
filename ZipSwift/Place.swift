d53cb7cc3c92a779a8535bdc4b62724a34887633
Rob Warner <rwarner@grailbox.com>
2015-02-19 13:02:12 -0500

    Configure RestKit and pull results for 32256

---
+++ ZipSwift/Place.swift
@@ -0,0 +1,17 @@
+//
+//  Place.swift
+//  ZipSwift
+//
+//  Created by Rob Warner on 2/18/15.
+//  Copyright (c) 2015 Coding in Shades. All rights reserved.
+//
+
+import Foundation
+
+@objc(Place)
+class Place : NSObject {
+  var city: String?
+  var state: String?
+  var latitude: String?
+  var longitude: String?
+}
--- ZipSwift/ViewController.swift
+++ ZipSwift/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  func configureRestKit() {
+    let url = NSURL(string: "http://api.zippopotam.us")
+    let client = AFHTTPClient(baseURL: url)
+    let manager = RKObjectManager(HTTPClient: client)
+    
+    let placeMapping = RKObjectMapping(forClass: Place.self)
+    placeMapping.addAttributeMappingsFromDictionary([
+      "place name": "city",
+      "state abbreviation": "state",
+      "latitude": "latitude",
+      "longitude": "longitude"
+    ])
+    
+    let responseDescriptor = RKResponseDescriptor(
+      mapping: placeMapping,
+      method: .GET,
+      pathPattern: "/us/:zipcode",
+      keyPath: "places",
+      statusCodes: NSIndexSet(index: 200)
+    )
+    manager.addResponseDescriptor(responseDescriptor)
+  }
+  
+  func loadPlaces(zipcode: String) {
+    let manager = RKObjectManager.sharedManager()
+    manager.getObjectsAtPath("/us/\(zipcode)",
+      parameters: nil,
+      success: { (operation, mappingResult) -> Void in
+        let places = mappingResult.array() as! [Place]
+        for place in places {
+          println("\(place.city), \(place.state)")
+        }
+      },
+      failure: { (operation, error) -> Void in
+        println("\(error)")
+      }
+    )
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    configureRestKit()
+    loadPlaces("32256")
   }
 
   override func didReceiveMemoryWarning() {
